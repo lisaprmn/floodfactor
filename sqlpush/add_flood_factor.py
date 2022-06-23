3624a504fdd9b05e00ff21a6d3ced0eb3b466ec5
Caroline Collin <Caroline.Collin@be.ey.com>
2022-06-24 00:27:14 +0200

    add function to get flooding risk from coordinates

--- sqlpush/add_flood_factor.py
+++ sqlpush/add_flood_factor.py
@@ -2,10 +2,17 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 from config import *
+from datetime import datetime as DT
 
 
     
 def add_risk_factor(df_addresses) :
+    """
+    add the risk factor from the map to a dataframe containing coordinate columns
+    input: the dataframe with coordinates
+    config: name of the coordinate columns, geographical referential used
+    """
+    
     print("\nImporting flooding risk map...")
     risk_map = gpd.read_file(floodingmap)
 
@@ -43,16 +50,50 @@ def add_risk_factor(df_addresses) :
 
     return res
 
+
+def get_risk_from_coordinates(lon, lat, plot_map = False) :
+
+    # import flooding risk map
+    print(f"\n{DT.now()} - Importing flooding risk map")
+    risk_map = gpd.read_file(floodingmap)
+
+    # convert coordinates to belgian system
+    df = pd.DataFrame([[lat, lon]], columns = ['lat', 'lon'])
+    geometry = [Point(xy) for xy in zip(df.lon, df.lat)]
+    gdf = gpd.GeoDataFrame(df, crs = "EPSG:4326", geometry = geometry).to_crs(crs)
     
+    # search on the map
+    risk_gdf = gpd.sjoin(gdf, risk_map, op = "within")
+
+    res = ""
+    if len(risk_gdf) == 0 :
+        res = "no risk" # no shape found in risk map
+    else :
+        alea = risk_gdf.TYPEALEA.values[0]
+        lvl = risk_gdf.RISK_LEVEL.values[0]
+        res = f"{lvl} risk"
+
+        if len(alea) > 1 :
+            res = f"{lvl} risk of type: {alea}"
+            
+    return gdf, risk_gdf, res
+
+
+
 if __name__ == "__main__" :
     from sqlstd import connectdb
     import os
 
-    data_folder = os.path.abspath(os.path.join(os.path.join(os.getcwd(), ".."), "data"))
-    print("Load flooding map...")
+    lat = 50.850164895152545
+    lon = 4.3390629560555265
+
+    gdf, risk_gdf, res = get_risk_from_coordinates(lon, lat, plot_map = False)
+    
+    #data_folder = os.path.abspath(os.path.join(os.path.join(os.getcwd(), ".."), "data"))
+    #print("Load flooding map...")
 
     # get addresses
-    df_addresses = pd.read_csv(inputfilepath, index_col=None, encoding="ISO-8859-1", low_memory=False).drop('address_id', axis = 1)
+    #df_addresses = pd.read_csv(inputfilepath, index_col=None, encoding="ISO-8859-1", low_memory=False).drop('address_id', axis = 1)
 
-    res = add_risk_factor(df_addresses)
+    #res = add_risk_factor(df_addresses)
     
