7fec69f6506b62d4f1c21e912eca2fc6f30f6548
Caroline Collin <Caroline.Collin@be.ey.com>
2022-07-04 21:12:37 +0200

    add a config file for parameters to build the map

---
+++ get_flooding_risk/config.py
@@ -0,0 +1,20 @@
+
+# path to folders
+
+data_folder = "C:\\Users\\ZA796CN\\OneDrive - EY\\Documents\\"
+img_folder = "C:\\Users\\ZA796CN\\OneDrive - EY\\Documents\\"
+
+
+
+# translation of the risk levels and types seen in different maps
+
+risk_level_dic = {"Aléa moyen" : "medium",
+                  "Aléa élevé" : "high",
+                  "Aléa faible" : "low",
+                  "Aléa très faible" : "very low",
+                  "Effectief overstromingsgevoelig gebied" : "effective",
+                  "Mogelijk overstromingsgevoelig gebied" : "potential"}
+
+type_alea_dic = {"Ruissellement" : "run-off",
+                 "Débordement & ruissellement" : "overflow & run-off",
+                 "Débordement" : "overflow"}
